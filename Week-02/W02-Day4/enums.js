bf67e5a6bb879b9bee6e4cffc6ace6dd0ea3846e
Ans Qazzafi <ans.qazzafi@bracketsltd.email>
2024-10-24 09:53:14 +0500

    updated

---
+++ Week-02/W02-Day4/enums.js
@@ -0,0 +1,21 @@
+//Catogarize student by grade level using enums
+var gradeLevel;
+(function (gradeLevel) {
+    gradeLevel["freshman"] = "freshman";
+    gradeLevel["sophomore"] = "sophomore";
+    gradeLevel["junior"] = "junior";
+    gradeLevel["senior"] = "senior";
+})(gradeLevel || (gradeLevel = {}));
+var FilteredStudents = [];
+function filterStudent(students, passingMarks) {
+    return students.filter(function (student) { return student.marks > passingMarks; });
+}
+var students = [
+    { name: "Ali", marks: 85, gradeLevel: gradeLevel.freshman },
+    { name: "Ans", marks: 75, gradeLevel: gradeLevel.sophomore },
+    { name: "yousuf", marks: 90, gradeLevel: gradeLevel.junior },
+    { name: "kamram", marks: 34, gradeLevel: gradeLevel.senior }
+];
+var filtered = filterStudent(students, 50);
+FilteredStudents.push.apply(FilteredStudents, filtered);
+console.log(FilteredStudents);
---
+++ Week-02/W02-Day4/enums.ts
@@ -0,0 +1,30 @@
+//Catogarize student by grade level using enums
+enum gradeLevel {
+    freshman="freshman",
+    sophomore="sophomore",
+    junior="junior",
+    senior= "senior"
+}
+
+interface StudentDetails {
+    name:string,
+    marks:number,
+    gradeLevel:gradeLevel
+}
+const FilteredStudents:StudentDetails[]=[]
+//generic function
+function filterStudent<T extends StudentDetails>(students:T[] , passingMarks:number):T[]{
+    return students.filter(student => student.marks > passingMarks) as T[]
+}
+const students:StudentDetails[] =[
+    { name: "Ali", marks: 85, gradeLevel: gradeLevel.freshman },
+    { name: "Ans", marks: 75, gradeLevel: gradeLevel.sophomore },
+    { name: "yousuf", marks: 90, gradeLevel: gradeLevel.junior },
+    { name: "kamram", marks: 34, gradeLevel: gradeLevel.senior }
+] 
+
+const filtered = filterStudent(students , 50)
+FilteredStudents.push(...filtered)
+console.log(FilteredStudents);
+
+
